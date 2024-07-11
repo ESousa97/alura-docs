7520b51bfbefa7173b8d5b17288178957aed35dc
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-07-10 21:36:27 -0300

    Finalizado as rotas de envio e salvamento de dados
    Pendente implementar o banco de dados MongoDB para não ter riscos
    de perder esses dados.

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,9 +1,50 @@
 import io from "./servidor.js";
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de Javascript..."
+    },
+    {
+        nome: "Node",
+        texto: "texto de Node..."
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de Socket.io..."
+    },
+];
+
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:", socket.id);
 
-    socket.on("texto_editor", (texto) => {
-        socket.broadcast.emit("texto_editor_clientes", texto);
+    socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
+        socket.join(nomeDocumento);
+
+        const documento = encontraDocumento(nomeDocumento);
+
+        if (documento) {
+
+            devolverTexto(documento.texto);
+        }
     })
+
+    socket.on("texto_editor", ({ texto, nomeDocumento }) => {
+    const documento = encontraDocumento(nomeDocumento);
+
+    if (documento) {
+        documento.texto = texto;
+        
+        socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+    }
+    
+    });
   });
+  
+  function encontraDocumento(nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome;
+    });
+
+    return documento;
+  };
